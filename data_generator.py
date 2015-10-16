afec3795249884c39e16924b937acbd863987ba7
Otto Wagner <otto@dialexa.com>
2015-10-16 18:35:57 -0500

    High level fleshing out of code.

--- data_generator.py
+++ data_generator.py
@@ -1,2 +1,24 @@
 __author__ = 'otto'
 
+"""
+This File generates our data points  It has three methods.
+One that generates training data.
+One that generates validation data
+One that visualizes training data.
+"""
+
+def generate_n_training_data_points( n = 100):
+    """
+    Todo. Generate n training data points and call it set T
+    Returned values will assume the form:
+    [(Class1,[x1,y1]),(Class2,[x2,y2]),...,(Classn,[xn,yn])]
+    """
+    return []
+
+def generate_n_validation_data_points(n = 100):
+    """
+    Todo. Generate n training data points and call it set V. Such that V union T is the null set.
+    Returned values will assume the form:
+    [(Class1,[x1,y1]),(Class2,[x2,y2]),...,(Classn,[xn,yn])]
+    """
+    return []
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 __author__ = 'otto'
 import data_generator
 
+training_data =  data_generator.generate_n_training_data_points(n = 100)
+validation_data = data_generator.generate_n_validation_data_points(n = 100)
+
+
+
