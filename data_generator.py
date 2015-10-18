245318c1c8fb1ff2f913084d132da3f4588cd204
Otto Wagner <otto@dialexa.com>
2015-10-17 21:15:09 -0500

    Worked a bit on getting my parabolic sigmoid training data generated.

--- data_generator.py
+++ data_generator.py
@@ -1,4 +1,9 @@
 __author__ = 'otto'
+import random
+import  numpy as np
+from matplotlib import pyplot as plt
+import pylab
+
 
 """
 This File generates our data points  It has three methods.
@@ -6,14 +11,25 @@ One that generates training data.
 One that generates validation data
 One that visualizes training data.
 """
+def plus_or_minus(term):
+    return random.randint(-term,term)
 
-def generate_n_training_data_points( n = 100):
+def squidmoid(inpx,stretch_factor_x = 100.00, stretch_factor_y = 200.00, random_interval_range = 200, yoffset = 500):
+    return  float(stretch_factor_y/(1 + np.exp(-((inpx-500)/stretch_factor_x)**2))) + plus_or_minus(random_interval_range) + yoffset
+
+def generate_n_training_data_points(y_off, n = 100,):
     """
     Todo. Generate n training data points and call it set T
     Returned values will assume the form:
     [(Class1,[x1,y1]),(Class2,[x2,y2]),...,(Classn,[xn,yn])]
     """
-    return []
+    stx = random.randint(200,300)
+    sty = random.randint(400,600)
+
+    y_offset = random.randint(0,1000)
+    xs =  [random.randint(0,1000) for x in range(n)]
+    ys =  [float(squidmoid(z,yoffset = y_off,stretch_factor_y = float(sty),stretch_factor_x= float(stx))) for z in xs]
+    return zip(xs, ys)
 
 def generate_n_validation_data_points(n = 100):
     """
@@ -22,8 +38,13 @@ def generate_n_validation_data_points(n = 100):
     [(Class1,[x1,y1]),(Class2,[x2,y2]),...,(Classn,[xn,yn])]
     """
     return []
+def static_var(varname, value):
+    def decorate(func):
+        setattr(func, varname, value)
+        return func
+    return decorate
 
-
+@static_var("counter", 0)
 def visualize_data(data = None):
     """
     Todo. Takes a list of lists. Then maps all of those lists on a 2d plot.
@@ -31,5 +52,11 @@ def visualize_data(data = None):
     A list of list contains len(list_of_list) * 2 different colorings.
     Visualization will be usefull in showing step by step by step evolution of isomomorphic transformation.
     """
+    visualize_data.counter += 1
+    x,y = zip(*data)
+    plt.scatter(x,y)
+    plt.show()
+
+
     pass
 
--- main.py
+++ main.py
@@ -6,19 +6,23 @@ from code_utils import *
 import theano
 import matplotlib
 
-training_data =  data_generator.generate_n_training_data_points(n = 100)
+training_data_a =  data_generator.generate_n_training_data_points(0,n = 1000)
+training_data_v =  data_generator.generate_n_training_data_points(500,n = 100)
+
 validation_data = data_generator.generate_n_validation_data_points(n = 100)
 
+data_generator.visualize_data(training_data_a + training_data_v)
 
 time = 0
 
 while True:
-    ## Should we kill the algorithm if there is a timeout or if the user asks so?
+    ## Should we kill the algorithm if there is a timeout or if the user asks us to?
     if end_algorithm_is_good_idea(time,userControl= False, discrete_timeout_limit= 999) is True:
         print "================= \nAlgorithm a kill\nIteration no." + str(time) + "\n================="
         break
     else:
         time = time + 1
-
     ########## The algorithm
+
+
 
